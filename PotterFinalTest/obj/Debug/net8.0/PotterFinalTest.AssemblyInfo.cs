605f8e98c5584d01e344ed34540bc90f225b431a
Davis Wood <51009602+SuperStik@users.noreply.github.com>
2025-04-30 14:22:35 -0700

    Add SQLite to solution (#2)

--- PotterFinalTest/obj/Debug/net8.0/PotterFinalTest.AssemblyInfo.cs
+++ PotterFinalTest/obj/Debug/net8.0/PotterFinalTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PotterFinalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0ad9846de6e47cde483b186ecbd02de6d72e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b90cfd2ddc375188a210c486df8e4d0b00dc1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PotterFinalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PotterFinalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
