b65fdf0ac0a0e751e4bdf44b43eef90209f13d11
Laurel Latt <latt@chapman.edu>
2025-05-06 16:19:04 -0700

    fixed some issues in HouseDescription

--- PotterFinal/HouseDescription.cs
+++ PotterFinal/HouseDescription.cs
@@ -2,17 +2,16 @@ namespace PotterFinal;
 
 using System.Collections.Generic;
 public class HouseDescription {
-	private SqliteConnection Connection;
 	private SqliteOps ops;
-	void HouseDescription() {
-		ops = new();
+	public HouseDescription() {
+		ops = new SqliteOps();
 	}
 
 	public string GetDescription(string housename) {
 		// who tf made this nasticide API??
-		Dictionary<string, string> parms = new();
+		Dictionary<string, string> parms = new Dictionary<string, string>();
 		parms.Add("name", housename);
-		List<string> desc = ops.SelectQueryWithParams("SELECT description FROM House WHERE name = :name LIMIT 1;", parms);
+		List<string> desc = ops.SelectQueryWithParams("SELECT description FROM House WHERE name = @name LIMIT 1;", parms);
 		if (desc.Count == 1)
 			return desc[0];
 		else {
