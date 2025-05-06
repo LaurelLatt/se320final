629e38077cccb3926c4cf0cd605ea47f797333ca
SuperStik <daviswood290@gmail.com>
2025-05-06 15:39:46 -0700

    FIXME: Add select query for HouseDescription

--- PotterFinal/HouseDescription.cs
+++ PotterFinal/HouseDescription.cs
@@ -1,5 +1,22 @@
-namespace DefaultNamespace;
+namespace PotterFinal;
 
+using System.Collections.Generic;
 public class HouseDescription {
-	
+	private SqliteConnection Connection;
+	private SqliteOps ops;
+	void HouseDescription() {
+		ops = new();
+	}
+
+	public string GetDescription(string housename) {
+		// who tf made this nasticide API??
+		Dictionary<string, string> parms = new();
+		parms.Add("name", housename);
+		List<string> desc = ops.SelectQueryWithParams("SELECT description FROM House WHERE name = :name LIMIT 1;", parms);
+		if (desc.Count == 1)
+			return desc[0];
+		else {
+			return null;
+		}
+	}
 }
